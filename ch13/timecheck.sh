934d6f46287a8919bce3086bd16423a45b484d63
John R Evans <John.Evans97@gmail.com>
2023-01-20 15:53:22 -0600

    Standard update in ch13---Fri Jan 20 15:53:21 CST 2023.

--- ch13/timecheck.sh
+++ ch13/timecheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/etc/mykiamol/ch13
+# cd /etc/mykiamol/ch13
 
 kubectl apply -f timecheck/
 
@@ -10,3 +10,13 @@ kubectl logs -l app=timecheck --all-containers -n kiamol-ch13-dev --tail 1
 
 kubectl wait --for=condition=ContainersReady pod -l app=timecheck -n kiamol-ch13-test
 
+# check those logs:
+kubectl logs -l app=timecheck --all-containers -n kiamol-ch13-test --tail 1
+
+exit 0
+
+Excerpt From
+Learn Kubernetes in a Month of Lunches
+Elton Stoneman
+https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewBook?id=0
+This material may be protected by copyright.
